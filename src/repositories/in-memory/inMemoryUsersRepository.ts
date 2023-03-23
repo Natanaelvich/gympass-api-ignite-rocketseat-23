6ccc43620e0353b11688914706ef65ce003f1f2f
Natanaelvich <taelima1997@gmail.com>
2023-03-22 23:22:03 -0300

    add user repository in memory

---
+++ src/repositories/in-memory/inMemoryUsersRepository.ts
@@ -0,0 +1,33 @@
+import { Prisma, User } from '@prisma/client'
+
+import { randomUUID } from 'node:crypto'
+import { UsersRepository } from '../users-repository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+      updated_at: new Date(),
+    }
+
+    this.items.push(user)
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+}
