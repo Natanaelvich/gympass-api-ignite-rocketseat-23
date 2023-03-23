610ce37626e5baa2c06eb5e27399236f00e23895
Natanaelvich <taelima1997@gmail.com>
2023-03-22 23:03:23 -0300

    refactor controller to no use try catch

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,4 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 import { RegisterUseCase } from '@/use-cases/register'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
@@ -13,18 +12,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { email, name, password } = registerBodySchema.parse(request.body)
 
-  try {
-    const usersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
+  const usersRepository = new PrismaUsersRepository()
+  const registerUseCase = new RegisterUseCase(usersRepository)
 
-    await registerUseCase.handle({ email, name, password })
-  } catch (error) {
-    if (error instanceof UserAlreadyExistsError) {
-      return reply.status(409).send({ message: error.message })
-    }
-
-    reply.status(500).send() // TODO: fix me
-  }
+  await registerUseCase.handle({ email, name, password })
 
   return reply.status(201).send()
 }
