eee9c76b10625fd7063971061d563de2b4644afa
Natanaelvich <taelima1997@gmail.com>
2023-03-22 23:22:15 -0300

    update test with repo memory

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,25 +1,11 @@
 import { expect, describe, it } from 'vitest'
 import { compare } from 'bcryptjs'
 import { RegisterUseCase } from './register'
-import { UsersRepository } from '@/repositories/users-repository'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/inMemoryUsersRepository'
 
 describe('Register Use Case', () => {
   it('should hash the password before saving it to the database', async () => {
-    const usersRepository: UsersRepository = {
-      async findByEmail() {
-        return null
-      },
-
-      async create(data) {
-        return {
-          id: '1',
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        }
-      },
-    }
+    const usersRepository = new InMemoryUsersRepository()
 
     const registerUseCase = new RegisterUseCase(usersRepository)
 
