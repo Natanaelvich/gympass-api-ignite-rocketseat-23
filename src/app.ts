f4c93bd5e5de1f5b5c1d73b50b7c7d8d6e4336cf
Natanaelvich <taelima1997@gmail.com>
2023-03-22 23:05:48 -0300

    refactor to use infer type

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import fastify, { FastifyError, FastifyReply, FastifyRequest } from 'fastify'
+import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { appRoutes } from './http/routes'
 
@@ -6,16 +6,14 @@ export const app = fastify()
 
 app.register(appRoutes)
 
-app.setErrorHandler(
-  (error: FastifyError, _request: FastifyRequest, reply: FastifyReply) => {
-    if (error instanceof ZodError) {
-      return reply
-        .status(400)
-        .send({ message: 'Validation error.', issues: error.format() })
-    }
-    console.log(error)
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
     return reply
-      .status(500)
-      .send({ message: 'Internal Server Error', statusCode: 500 })
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
   }
-)
+  console.log(error)
+  return reply
+    .status(500)
+    .send({ message: 'Internal Server Error', statusCode: 500 })
+})
