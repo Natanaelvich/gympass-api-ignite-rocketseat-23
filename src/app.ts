8726f5219d3d91c60303248a0706ec20e3c96107
Natanaelvich <taelima1997@gmail.com>
2023-03-22 23:02:41 -0300

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
-import fastify from 'fastify'
+import fastify, { FastifyError, FastifyReply, FastifyRequest } from 'fastify'
+import { ZodError } from 'zod'
 import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler(
+  (error: FastifyError, _request: FastifyRequest, reply: FastifyReply) => {
+    if (error instanceof ZodError) {
+      return reply
+        .status(400)
+        .send({ message: 'Validation error.', issues: error.format() })
+    }
+    console.log(error)
+    return reply
+      .status(500)
+      .send({ message: 'Internal Server Error', statusCode: 500 })
+  }
+)
