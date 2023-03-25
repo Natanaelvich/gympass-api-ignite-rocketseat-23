302703f6a3432cf76e32db7c3184b6efa4a76499
Natanaelvich <taelima1997@gmail.com>
2023-03-24 22:46:40 -0300

    add PrismaCheckInsRepository

---
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -0,0 +1,34 @@
+import { prisma } from '@/lib/prisma'
+import { CheckIn, Prisma } from '@prisma/client'
+import { CheckInsRepository } from '../check-ins-repository'
+
+export class PrismaCheckInsRepository implements CheckInsRepository {
+  async findById(id: string) {
+    const checkIn = await prisma.checkIn.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return checkIn
+  }
+
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = await prisma.checkIn.create({
+      data,
+    })
+
+    return checkIn
+  }
+
+  async save(data: CheckIn) {
+    const checkIn = await prisma.checkIn.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    })
+
+    return checkIn
+  }
+}
