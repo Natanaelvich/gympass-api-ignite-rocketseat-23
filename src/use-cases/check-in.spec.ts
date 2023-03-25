6ade907c84bc9569e0fd1d351df10d6b4b7af4e7
Natanaelvich <taelima1997@gmail.com>
2023-03-25 19:32:09 -0300

    add test distancy gym

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,16 +1,30 @@
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { Decimal } from '@prisma/client/runtime'
 
 import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { CheckInUseCase } from './check-in'
+import { MaxDistanceError } from './errors/max-distance-error'
 import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-ins-error'
 
 let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
 let sut: CheckInUseCase
 
 describe('CheckIn Use Case', () => {
   beforeEach(async () => {
     checkInsRepository = new InMemoryCheckInsRepository()
-    sut = new CheckInUseCase(checkInsRepository)
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+    await gymsRepository.create({
+      id: 'gym-01',
+      title: 'JavaScript Gym',
+      description: '',
+      latitude: -15.79152,
+      longitude: -47.8913192,
+      phone: '',
+    })
 
     vi.useFakeTimers()
   })
@@ -72,5 +86,27 @@ describe('CheckIn Use Case', () => {
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.items.push({
+      id: 'gym-02',
+      title: 'JavaScript Gym',
+      description: '',
+      latitude: new Decimal(-15.7336978),
+      longitude: new Decimal(-47.8757838),
+      phone: '',
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-02',
+        userId: 'user-01',
+        userLatitude: -15.79152,
+        userLongitude: -47.8913192,
+      })
+    ).rejects.toBeInstanceOf(MaxDistanceError)
   })
 })
