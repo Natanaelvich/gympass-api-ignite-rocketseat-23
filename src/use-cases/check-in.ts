ff2e1ad756162ec59db8c9311e769ada44ab414e
Natanaelvich <taelima1997@gmail.com>
2023-03-24 22:46:50 -0300

    add CheckInUseCaseRequest

---
+++ src/use-cases/check-in.ts
@@ -0,0 +1,31 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { CheckIn } from '@prisma/client'
+
+interface CheckInUseCaseRequest {
+  userId: string
+  gymId: string
+  userLatitude: number
+  userLongitude: number
+}
+
+interface CheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class CheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    gymId,
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.create({
+      user_id: userId,
+      gym_id: gymId,
+    })
+
+    return {
+      checkIn,
+    }
+  }
+}
