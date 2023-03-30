3a5155964eba760c3a5824defd22f09a34b9e6a7
Natanaelvich <taelima1997@gmail.com>
2023-03-29 23:21:59 -0300

    chore: add util createAndAuthenticateUser

---
+++ src/utils/test/create-and-authenticate-user.ts
@@ -0,0 +1,25 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
+import request from 'supertest'
+
+export async function createAndAuthenticateUser(app: FastifyInstance) {
+  await prisma.user.create({
+    data: {
+      name: 'John doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    },
+  })
+
+  const authResponse = await request(app.server).post('/session').send({
+    email: 'johndoe@example.com',
+    password: '123456',
+  })
+
+  const { token } = authResponse.body
+
+  return {
+    token,
+  }
+}
