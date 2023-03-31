8e445ef42b024b3845f4e10d5785535f31fa8cc5
Natanaelvich <taelima1997@gmail.com>
2023-03-30 23:31:09 -0300

    chore: update with role and refresh token on cookie

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -19,23 +19,40 @@ export async function authenticate(
 
     const { user } = await authenticateUseCase.execute({ email, password })
 
-    const token = await reply.jwtSign({
-      sign: {
-        sub: user.id,
+    const token = await reply.jwtSign(
+      {
+        role: user.role,
       },
-    })
+      {
+        sign: {
+          sub: user.id,
+        },
+      }
+    )
 
-    const refreshToken = await reply.jwtSign({
-      sign: {
-        sub: user.id,
-        expiresIn: '7d',
+    const refreshToken = await reply.jwtSign(
+      {
+        role: user.role,
       },
-    })
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      }
+    )
 
-    return reply.status(200).send({
-      token,
-      refreshToken,
-    })
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true, // HTTPs
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
+      })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message })
