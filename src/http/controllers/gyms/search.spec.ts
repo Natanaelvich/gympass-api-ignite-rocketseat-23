bc807faec3821147e7ad18081dfedc999878ff50
Natanaelvich <taelima1997@gmail.com>
2023-04-01 09:29:33 -0300

    chore: add search gyms controller and test

---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,55 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/createAndAuthenticateUser'
+
+describe('Search Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to search gyms by title', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description',
+        phone: '1199999999',
+        latitude: -15.79152,
+        longitude: -47.8913192,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'TypeScript Gym',
+        description: 'Some description',
+        phone: '1199999999',
+        latitude: -15.79152,
+        longitude: -47.8913192,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        q: 'JavaScript',
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toBe(200)
+    expect(
+      response.body.gyms.some(
+        (gym: { title: string }) => gym.title === 'JavaScript Gym'
+      )
+    ).toBe(true)
+  })
+})
